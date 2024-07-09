901457fc7e4f8a97e5507f6d4618cdd572a24c1a
kiranfast <kiranfast008@gmail.com>
2024-07-09 19:46:16 +0530

    shopping

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,53 @@ import Product from './container/product'
 function App() {
     let data = [
       {
-        productname:"Samsung Mobile",
-        rate:"15,000 $ - 20,000 $ ",
-        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaHQYwJ8dZhyBvqZ-xOJVS0cAIDu0HTdp81A&s" 
-      },
-      {
-        productname:"Perfume",
-        rate:"400 $ - 200 $ " ,
-        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4WSMkPcePF8F-lWHsN3Rf86T8wKYfH8qxCA&s"
-      },
-      {
-        productname:"Glass Decorations",
-        rate:"50 $ - 100 $ " ,
-        pic:"https://m.media-amazon.com/images/I/61mJ0ZJYXjL._AC_UF894,1000_QL80_.jpg"
-      },
-      {
-        productname:"Bingo Potato Chips",
-        rate:"250 $ - 500 $ ",
-        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmgYhQ-4I-MyKSRHCXFDR9uL1z9Xb71JXPwQ&s"
-      },
-      {
         productname:"Diary Milk Silk",
-        rate:"10 $ - 200 $ ",
-        pic: "https://assets.winni.in/c_limit,dpr_1,fl_progressive,q_80,w_1000/81619_two-dairy-milk-chocolate-with-soft-toy.jpeg"
+        rate:"100 $ - 400 $ ",
+        pic: "https://assets.winni.in/c_limit,dpr_1,fl_progressive,q_80,w_1000/81619_two-dairy-milk-chocolate-with-soft-toy.jpeg",
+        strike:"$500",
       },
       {
-        productname:"Kitchen Appliances",
-        rate:"40 $ - 100 $ ",
-        pic:"https://leveltalk.wordpress.com/wp-content/uploads/2014/08/group-stainless-steel-kitchen-items-13989128.jpg",
+        productname:"Induction Stove",
+        rate:"400 $ - 900 $ ",
+        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUGM-1CtdcLVyU3OMqrdmCsI8Aacs6YrV1Ww&s",
+         strike:"$1000",
       },
       {
         productname:"Golden retriever",
-        rate:"1500 $ - 200 $ ",
-        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTecL89qMNIB7_-kmyfGMVmJgNj3xu2_ReP0w&s"
+        rate:"1500 $ - 2200 $ ",
+        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTecL89qMNIB7_-kmyfGMVmJgNj3xu2_ReP0w&s",
+        strike:"$10,000",
       },
       {
         productname:"Water Bottle",
-        rate:"15 $ - 20 $ ",
-        pic:"https://m.media-amazon.com/images/I/71mDwwqFHLL._AC_UF1000,1000_QL80_.jpg"
+        rate:"800 $ - 900 $ ",
+        pic:"https://m.media-amazon.com/images/I/71mDwwqFHLL._AC_UF1000,1000_QL80_.jpg",
+        strike:"$5000",
       },
+      {
+        productname:"Toys For The Kids",
+        rate:"500 $ - 750 $ ",
+        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4cMIWE15IzMXjb_Q28GV2JqciSPpCHSeKcg&s",
+        strike:"$3000",
+      },
+      {
+        productname:"Technical Mouse",
+        rate:"140 $ - 600 $ ",
+        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeRBz49IHFH5JPUKICi4sdkk4KO5Tayjde2w&s",
+        strike:"$800",
+      },
+      {
+        productname:"Wireless Mouse",
+        rate:"150 $ - 200 $",
+        pic:"https://m.media-amazon.com/images/I/61Mk3YqYHpL._AC_UF894,1000_QL80_.jpg",
+        strike:"$1500",
+      },
+      {
+        productname:" Advanced Mouse",
+        rate:"1500 $ - 2000 $ ",
+        pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfgzaeUCYnMyGa-lis6tecjVAW6X2DWJPrVg&s",
+        strike:"$6000"
+      }
     ]
     let [cart,setcart]=useState(0)
   return <>
@@ -51,8 +59,8 @@ function App() {
   <header class="bg-dark py-5">
             <div className="container px-4 px-lg-5 my-5">
                 <div className="text-center text-white">
-                    <h1 className="display-4 fw-bolder">Amazon Big Billion Days</h1>
-                    <p className="lead fw-normal text-white-50 mb-0">Have a Happy Shopping</p>
+                    <h1 className="display-4 fw-bolder">Shop in style</h1>
+                    <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
                 </div>
             </div>
         </header>
@@ -68,7 +76,9 @@ function App() {
                             </div>
                         </div>
                         </section>
-
+                        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
   </>
 }
 
--- src/container/product.jsx
+++ src/container/product.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
 
 export default function product({cart,setcart,data}) {
     let [toggle,settoggle] = useState(true)
-
+    const [rating, setRating] = useState(0)
+    const handleRating = (rate) => {
+        setRating(rate)
+      }
   return<>
   <div className="col mb-5">
                         <div className="card h-100">
                           
-                            <img className="card-img-top" src={data.pic} alt="..." />
+                            <img className="card-img-top" src={data.pic} alt="..."/>
                         
                             <div className="card-body p-4">
                                 <div className="text-center">
                                
                                     <h5 className="fw-bolder">{data.productname}</h5>
-                                 
-                                    {data.rate}
+                                    <strike style={{color:'grey'}}> {data.strike}</strike>
+                                    <br/>{data.rate} 
                                 </div>
+                                <Rating onClick={handleRating} initialValue={rating} />
                             </div>
                             <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
                             {
